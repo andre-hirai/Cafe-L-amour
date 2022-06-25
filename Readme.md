2fbf82aa376dde5882faf7eb71565484909dc0ca
Andre Hirai <andrehiraifx@hotmail.com>
2022-06-25 13:51:31 -0300

    Café L'amour

---
+++ Readme.md
@@ -0,0 +1,15 @@
+Projeto realizado a partir do completo zero.
+Desde idealização do projeto, a Identidade visual da "empresa"
+Uma experiência completa, desde o design de interface ao design da web.
+
+Projeto inteiramente codado sem uso de Frameworks, famoso 'na unha'
+O Design foi feito através do Adobe Illustrator
+As imagens foram adaptadas para a Web através do Adobe Photoshop
+
+
+*Tempo investido no projeto, 4 dias
+
+Linguagens usadas:
+HTML, CSS e JS
+
+- Studio Made Art - 2022
---
+++ script.js
@@ -0,0 +1,38 @@
+var torra=document.querySelector("h3.titulo-sec5")
+var desc=document.querySelector("p.descricao-sec5")
+var bnt=document.querySelector("input#btn-sec5")
+var claro=document.querySelector("div#bloco-claro")
+var medio=document.querySelector("div#bloco-medio")
+var escuro=document.querySelector("div#bloco-escuro")
+
+function clara(){
+    var claro=document.querySelector("div#bloco-claro")
+    torra.innerText= "TORRA CLARA"
+    torra.style.color = "#f59727"
+    desc.innerText="Na torra clara o café é menos amargo. A suavidade, a acidez e o aroma se destacam neste tipo de torra. Este café é classificado como café especial."
+    desc.style.color="#f59727"
+    claro.style.height= '30px'
+}
+function media(){
+    torra.innerText="TORRA MÉDIA"
+    torra.style.color = "#e75e30"
+    desc.innerText="Na torra média, como o próprio nome sugere, a acidez, os açúcares e o amargor ficam bem balanceados. A torra média é indicada para cafés coados."
+    desc.style.color="#e75e30"
+    medio.style.height= '30px'
+}
+function escura(){
+    torra.innerText="TORRA ESCURA"
+    torra.style.color = "#ff3f29"
+    desc.innerText="Na torra escura, o café fica forte ou extraforte e por isso a amargura fica bastante presente. Vale destacar que este é o ponto de torra mais complexo por conta do risco de queimar os grãos."
+    desc.style.color = "#ff3f29"
+    escuro.style.height= '30px'
+}
+function saiu(){
+    claro.style.height= '15px'
+}
+function saiu2(){
+    medio.style.height= '15px'
+}
+function saiu3(){
+    escuro.style.height= '15px'
+}
